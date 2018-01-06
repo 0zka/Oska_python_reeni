98fd6e54e634f47b24c3f5cd400f3e3489254967
ozzzka <oskapakarinen@hotmail.com>
2018-01-06 12:49:16 +0200

    Bookprog_gittiin_v1

---
+++ bookprog.py
@@ -0,0 +1,117 @@
+"""
+Ohjelma, joka sisältää seuraavat kirjatiedot:
+
+Kirjailija, Nimi, Vuosi, ISBN
+
+Käyttäjä voi:
+
+Nähdä kaikki kirjat
+Etsiä kirjan
+Lisätä kirjan
+Päivittää kirjan
+Poistaa
+Sulkea
+"""
+
+from tkinter import *
+import bookbackend
+
+def get_selected_row(event):
+    global selected_tuple
+    if len(list1.curselection()) > 0:
+        index = list1.curselection()[0]
+        selected_tuple = list1.get(index)
+        e1.delete(0,END)
+        e1.insert(END,selected_tuple[1])
+        e2.delete(0,END)
+        e2.insert(END,selected_tuple[2])
+        e3.delete(0,END)
+        e3.insert(END,selected_tuple[3])
+        e4.delete(0,END)
+        e4.insert(END, selected_tuple[4])
+
+
+def view_command():
+    list1.delete(0,END)
+    for row in bookbackend.view():
+         list1.insert(END, row)
+
+def search_command():
+    list1.delete(0,END)
+    for row in bookbackend.search(title_text.get(), author_text.get(), year_text.get() ,isbn_text.get()):
+        list1.insert(END, row)
+
+def add_command():
+    bookbackend.insert(title_text.get(), author_text.get(), year_text.get() ,isbn_text.get() )
+    list1.delete(0,END)
+    list1.insert(END, (title_text.get(), author_text.get(), year_text.get() ,isbn_text.get() ))
+
+def delete_command():
+    bookbackend.delete(selected_tuple[0])
+
+def update_command():
+    bookbackend.update(selected_tuple[0], title_text.get(), author_text.get(), year_text.get() ,isbn_text.get())
+
+
+window = Tk()
+
+window.wm_title("Kirjakauppa")
+
+l1 = Label(window, text="Nimi")
+l1.grid(row = 0, column = 0 )
+
+l2 = Label(window, text="Kirjailija")
+l2.grid(row = 0, column = 2 )
+
+l3 = Label(window, text="Vuosi")
+l3.grid(row = 1, column = 0 )
+
+l4 = Label(window, text="ISBN")
+l4.grid(row = 1, column = 2 )
+
+title_text = StringVar()
+e1 = Entry( window, textvariable = title_text )
+e1.grid(row = 0, column = 1)
+
+author_text = StringVar()
+e2 = Entry( window, textvariable = author_text )
+e2.grid(row = 0, column = 3)
+
+year_text = StringVar()
+e3 = Entry( window, textvariable = year_text )
+e3.grid(row = 1, column = 1)
+
+isbn_text = StringVar()
+e4 = Entry( window, textvariable = isbn_text )
+e4.grid(row = 1, column = 3)
+
+list1 = Listbox(window, height = 6, width = 35)
+list1.grid(row = 2, column = 0, rowspan = 6, columnspan = 2)
+
+scrollbar1 = Scrollbar(window)
+scrollbar1.grid(row = 2, column = 2, rowspan = 6, sticky='ns')
+
+list1.configure(yscrollcommand = scrollbar1.set)
+scrollbar1.configure(command = list1.yview)
+
+list1.bind('<<ListboxSelect>>', get_selected_row )
+
+b1 = Button(window, text = "Näytä kaikki", width = 12, command=view_command)
+b1.grid(row = 2, column =3)
+
+b2 = Button(window, text = "Etsi nimike", width = 12, command =search_command)
+b2.grid(row = 3, column =3)
+
+b3 = Button(window, text = "Lisää nimike", width = 12, command=add_command)
+b3.grid(row = 4, column =3)
+
+b4 = Button(window, text = "Päivitä valittu", width = 12, command=update_command)
+b4.grid(row = 5, column =3)
+
+b5 = Button(window, text = "Poista valittu", width = 12, command=delete_command)
+b5.grid(row = 6, column =3)
+
+b6 = Button(window, text = "Sulje", width = 12, command=window.destroy)
+b6.grid(row = 7, column =3)
+
+window.mainloop()
